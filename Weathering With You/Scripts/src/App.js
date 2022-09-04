36901ce11a9f827ac430ec39b0d1408de1bb3959
akilaust <54270247+fahimar@users.noreply.github.com>
2022-09-05 03:42:22 +0600

    Add files via upload
    indexs and App

---
+++ Weathering With You/Scripts/src/App.js
@@ -0,0 +1,22 @@
+﻿import React from "react";
+import Dashboard from '../../Scripts/src/components/Dashboard.js'
+
+function App(props) {
+    
+
+    return (
+        <div className="App">
+            <Dashboard />
+        </div>
+    );
+}
+
+export default App;
+
+
+
+
+
+
+
+
---
+++ Weathering With You/Scripts/src/index.js
@@ -0,0 +1,7 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from '../../Scripts/src/App.js';
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
