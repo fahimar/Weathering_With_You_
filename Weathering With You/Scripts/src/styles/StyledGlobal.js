72e006eafa710b4a603095b09e2cd28d372b2937
akilaust <54270247+fahimar@users.noreply.github.com>
2022-09-05 03:41:47 +0600

    Add files via upload

---
+++ Weathering With You/Scripts/src/styles/StyledGlobal.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+*{
+    box-sizing:border-box;
+}
+body{
+    font-size:20px;
+    font-family: "AvertaStd-Thin";
+    background:${props => props.theme.bgContent};
+    margin:0;
+    margin-top:20px
+    padding:0;
+    line-height:1;
+}`;
+export default GlobalStyle;
---
+++ Weathering With You/Scripts/src/styles/index.js
@@ -0,0 +1,23 @@
+import StyledGlobal from '../../../Scripts/src/styles/StyledGlobal.js';// './StyledGlobal';
+import StyledHeader from '../../../Scripts/src/styles/StyledHeader.js';
+import StyledContainer from '../../../Scripts/src/styles/StyledContainer.js';
+import StyledHighlights from '../../../Scripts/src/styles/StyledHighlights.js';
+import StyledSidebar from '../../../Scripts/src/styles/StyledSidebar.js';
+import StyledToday from '../../../Scripts/src/styles/StyledToday.js';
+import StyledWeek from '../../../Scripts/src/styles/StyledWeek.js';
+import StyledDashboard from '../../../Scripts/src/styles/StyledDashboard.js';
+import StyledSpinner from '../../../Scripts/src/styles/StyledSpinner.js';
+import StyledSpinnerContainer from '../../../Scripts/src/styles/StyledSpinnerContainer.js';
+
+export {
+    StyledGlobal,
+    StyledHeader,
+    StyledContainer,
+    StyledHighlights,
+    StyledSidebar,
+    StyledWeek,
+    StyledToday,
+    StyledDashboard,
+    StyledSpinner,
+    StyledSpinnerContainer 
+};
