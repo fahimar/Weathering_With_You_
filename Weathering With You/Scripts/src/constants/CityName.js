e59b2f2dfc8b221d6ef33229633660cad8d72cb3
akilaust <54270247+fahimar@users.noreply.github.com>
2022-09-05 03:36:15 +0600

    Add files via upload
    constants\colors

---
+++ Weathering With You/Scripts/src/constants/CityName.js
@@ -0,0 +1,230 @@
+﻿export default`
+Jaipur
+Jakarta
+Jeddah
+Jerusalem
+Johannesburg
+Juarez
+Juba
+Kabul
+Kaduna
+Kampala
+Kano
+Kanpur
+Kaohsiung
+Karachi
+Karaj
+Kathmandu
+Kawasaki
+Kharkiv
+Khartoum
+Khulna
+Kigali
+Kingsburg
+Kingston
+Kingstown
+Kinshasa
+Kobe
+Kolkata
+Kota Bharu
+Kowloon
+Kuala Lumpur
+Kumasi
+Kuwait
+Kyiv
+Kyoto
+La Paz
+Lagos
+Lahore
+Libreville
+Lilongwe
+Lima
+Lisbon
+Ljubljana
+Lome
+London
+Los Angeles
+Luanda
+Lubumbashi
+Lusaka
+Luxembourg
+Macau
+Madrid
+Majuro
+Makassar
+Malabo
+Male
+Mamoudzou
+Managua
+Manama
+Manaus
+Manila
+Maputo
+Maracaibo
+Maracay
+Mariehamn
+Marigot
+Maseru
+Mashhad
+Mbabane
+Mecca
+Medan
+Medellin
+Medina
+Melbourne
+Mexico City
+Miami
+Minsk
+Mogadishu
+Monaco
+Monrovia
+Montevideo
+Montreal
+Moroni
+Moscow
+Mosul
+Multan
+Mumbai
+Muscat
+N'Djamena
+Nagoya
+Nairobi
+Nanchong
+Nanjing
+Nassau
+Nay Pyi Taw
+New York
+Niamey
+Nicosia
+Nouakchott
+Noumea
+Novosibirsk
+Nuku'alofa
+Nur-Sultan
+Nuuk
+Oranjestad
+Osaka
+Oslo
+Ottawa
+Ouagadougou
+Pago Pago
+Palembang
+Palo Alto
+Panama
+Papeete
+Paramaribo
+Paris
+Perth
+Philadelphia
+Phnom Penh
+Phoenix
+Podgorica
+Port Louis
+Port Moresby
+Port of Spain
+Port-Vila
+Port-au-Prince
+Porto Alegre
+Porto-Novo
+Prague
+Praia
+Pretoria
+Pristina
+Puebla
+Pune
+Pyongyang
+Quezon City
+Quito
+Rabat
+Rawalpindi
+Recife
+Reykjavik
+Riga
+Rio de Janeiro
+Riyadh
+Road Town
+Rome
+Roseau
+Saint George's
+Saint Helier
+Saint John's
+Saint Peter Port
+Saint Petersburg
+Saint-Denis
+Saint-Pierre
+Saipan
+Salvador
+San Antonio
+San Diego
+San Francisco
+San Jose
+San Juan
+San Marino
+San Salvador
+Sanaa
+Santa Cruz de la Sierra
+Santiago
+Santo Domingo
+Sao Paulo
+Sao Tome
+Sapporo
+Sarajevo
+Seattle
+Semarang
+Seoul
+Shanghai
+Sharjah
+Shenzhen
+Singapore
+Skopje
+Sofia
+South Tangerang
+Soweto
+Stockholm
+Sucre
+Surabaya
+Surat
+Suva
+Sydney
+Tabriz
+Taipei
+Tallinn
+Tangerang
+Tarawa
+Tashkent
+Tbilisi
+Tegucigalpa
+Tehran
+Tel Aviv
+Thimphu
+Tianjin
+Tijuana
+Tirana
+Tokyo
+Toronto
+Torshavn
+Tripoli
+Tunis
+Ulan Bator
+Vaduz
+Valencia
+Valletta
+Vancouver
+Victoria
+Vienna
+Vientiane
+Vilnius
+Warsaw
+Washington
+Wellington
+Willemstad
+Windhoek
+Wuhan
+Xi'an
+Yamoussoukro
+Yangon
+Yaounde
+Yekaterinburg
+Yerevan
+Yokohama
+Zagreb`.split('\n')
---
+++ Weathering With You/Scripts/src/constants/index.js
@@ -0,0 +1,6 @@
+import {themeLight, themeDark} from '../../../Scripts/src/constants/colors/index.js';
+
+export {
+    themeLight,
+    themeDark
+};
