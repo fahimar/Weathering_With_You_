bec8e4e0a045906790fac317f70eeabd1816cbd5
akilaust <54270247+fahimar@users.noreply.github.com>
2022-09-05 02:56:58 +0600

    Add files via upload
    app_data file

---
+++ Weathering With You/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Weathering_with_You_
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
